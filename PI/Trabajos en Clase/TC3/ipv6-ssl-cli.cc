7cf05429d59ec691c3811438457f9b6611e1a099
Pablo Rodriguez Navarro <44807688+Pochix14@users.noreply.github.com>
2023-03-24 16:49:35 -0600

    TC3: SSL
    Socket with SSL connection on TCP IPv4 & IPv6 mode

---
+++ PI/Trabajos en Clase/TC3/ipv6-ssl-cli.cc
@@ -0,0 +1,34 @@
+/**
+  *  Ejemplo de sockets con SSL
+  *
+  *  Compilar con:
+  *     g++ -g Socket.cc ipv4-ssl-cli.cc -lssl -lcrypto -o ssl-ipv4-ssl-cli.out
+  *
+ **/
+
+#include <stdio.h>
+#include <string.h>
+
+#include "Socket.h"
+
+#define SIZE 1024
+
+int main( int argc, char * argv[] ) {
+   char * os = (char *) "fe80::194d:4e78:60c:fedc%eno1";
+   char * reqOS = (char *) "GET /lego/figure=octupus HTTP/1.1\r\nhost: redes.ecci\r\n\r\n";
+   Socket s( 's', true );	// Create a new stream socket for IPv6
+   char a[ SIZE ];
+   int count;
+
+   memset( a, 0, SIZE );
+   s.initSSL();
+   s.SSLConnect( os, (char *) "https" );
+   s.SSLWrite(  (void *) reqOS, strlen( reqOS ) );
+   while (  (count = s.SSLRead( a, SIZE )) > 0 ) {
+      printf( "%s\n", a );
+      memset( a, 0, SIZE );
+   }
+
+   printf( "%s\n", a );
+
+}
